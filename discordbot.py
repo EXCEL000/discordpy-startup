be9728941b20b495552b79c75e7a60dc9f638bc9
EXCEL000 <61957980+EXCEL000@users.noreply.github.com>
2021-07-13 20:40:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,28 @@
 from discord.ext import commands
+from googletrans import Translator
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+translator = Translator()
 
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+async def on_ready():
+    print("on_ready")
+    print(discord.__version__)
+    text_channels.send("Translator is Enabled! | 翻訳機能が有効になりました！")
 
+async def on_message(message):
+    if translator.detect(message) == (lang = en, confidence > 0.9):
+        await  message.channel.send(translator.translate(message,src='en', dest='jp'))
+    elif translator.detect(message) == (lang=jp, confidence > 0.9):
+        await  message.channel.send(translator.translate(message,src='jp', dest='en'))
+    else:
+        await  message.channel.send("please talk in English or Japanese")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
 
 
 bot.run(token)
